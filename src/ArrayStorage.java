e88920c0cff0ca6da320fce0b9cc90c50d9a0567
kormachevt <kormachev.t@gmail.com>
2019-12-01 18:25:37 +0300

    changed vars naming, operators fix; delete() counter fix;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,58 +3,57 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int firstEmptySpotIndex = 0;
+    private int size = 0;
 
     void clear() {
-        for (int i = 0; i < firstEmptySpotIndex; i++) {
-            this.storage[i] = null;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
-        firstEmptySpotIndex = 0;
+        size = 0;
     }
 
     void save(Resume resume) {
         if (resume.uuid != null) {
-            this.storage[firstEmptySpotIndex] = resume;
-            firstEmptySpotIndex += 1;
+            storage[size] = resume;
+            size++;
         } else {
-            System.out.println("Empty resumes now allowed!");
+            System.out.println("Empty resumes are not allowed!");
         }
     }
 
     Resume get(String uuid) {
-        int indexOfResume = indexByUuid(uuid);
-        if (indexOfResume > 0) {
-            return this.storage[indexOfResume];
+        int index = indexByUuid(uuid);
+        if (index > 0) {
+            return storage[index];
         }
         return null;
     }
 
     void delete(String uuid) {
-        int indexOfResumeToBeDeleted = indexByUuid(uuid);
-        this.storage[indexOfResumeToBeDeleted] = null;
-        if (firstEmptySpotIndex + 1 - indexOfResumeToBeDeleted >= 0) {
-            System.arraycopy(this.storage, indexOfResumeToBeDeleted + 1, this.storage, indexOfResumeToBeDeleted,
-                    firstEmptySpotIndex + 1 - indexOfResumeToBeDeleted);
+        int index = indexByUuid(uuid);
+        storage[index] = null;
+        if (size + 1 - index >= 0) {
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
         }
-        firstEmptySpotIndex -= 1;
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[firstEmptySpotIndex];
-        System.arraycopy(this.storage, 0, resumes, 0, firstEmptySpotIndex);
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
     }
 
     int size() {
-        return firstEmptySpotIndex + 1;
+        return size + 1;
     }
 
     private int indexByUuid(String uuid) {
-        for (int i = 0; i < firstEmptySpotIndex; i++) {
-            if (this.storage[i].uuid.equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return i;
             }
         }
