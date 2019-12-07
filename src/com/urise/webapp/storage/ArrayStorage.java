5c55fc23af3486f3994433987c379595eecaacb1
wardruid <wardruid@yandex.ru>
2019-12-07 12:16:32 +0300

    Added package structure

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,19 +1,23 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume resume) {
-        if (resume.uuid != null) {
+    public void save(Resume resume) {
+        if (resume.getUuid() != null) {
             storage[size] = resume;
             size++;
         } else {
@@ -21,7 +25,7 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = indexByUuid(uuid);
         if (index >= 0) {
             return storage[index];
@@ -29,7 +33,7 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = indexByUuid(uuid);
         int leftover = size - index - 1;
         if (index >= 0) {
@@ -41,23 +45,23 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[size];
         System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
     private int indexByUuid(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }
-        System.out.println(String.format("No Resume with [uuid: %s] was found", uuid));
+        System.out.println(String.format("No com.urise.webapp.model.Resume with [uuid: %s] was found", uuid));
         return -1;
     }
 }
