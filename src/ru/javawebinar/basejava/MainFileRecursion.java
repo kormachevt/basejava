848a09ef33968a9d51c96304f241b7b9d026bea1
Tim Kormachev <tkormachev@alfabank.ru>
2020-03-07 16:19:55 +0300

    HW9: print files in dir with offset

--- src/ru/javawebinar/basejava/MainFileRecursion.java
+++ src/ru/javawebinar/basejava/MainFileRecursion.java
@@ -1,32 +1,34 @@
 package ru.javawebinar.basejava;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainFileRecursion {
 
     public static void main(String[] args) {
-        String path = "./src/ru/javawebinar/basejava/";
-        File dir = new File(path);
-        printFileNamesInDir(dir);
+        String dir = "./src";
+        Path path = Paths.get(dir);
+        StringBuffer sb = new StringBuffer();
+        printFileNamesInDir(path, sb);
     }
 
-    public static  void printFileNamesInDir(File dir) {
-        if (dir.isDirectory()) {
-            System.out.println(dir.getName());
-            File[] list = dir.listFiles();
-            if (list != null) {
-                for (File file : list) {
-                    printFileNamesInDir(file);
+    public static void printFileNamesInDir(Path path, StringBuffer sb){
+        String offset = "   ";
+        try {
+            Files.list(path).forEachOrdered((path1) -> {
+                if(Files.isDirectory(path1)){
+                    System.out.println(sb + path1.getFileName().toString());
+                    sb.append(offset);
+                    printFileNamesInDir(path1, sb);
+                } else {
+                    System.out.println(sb + path1.getFileName().toString());
                 }
-            }
-        } else if (dir.isFile()) {
-            printFileName(dir);
-        } else {
-            throw new RuntimeException("Something went wrong");
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    private static void printFileName(File file) {
-        System.out.println("    " + file.getName());
+        sb.delete(0,offset.length());
     }
 }
