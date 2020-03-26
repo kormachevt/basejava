6fe9a1184339c651d0e8a35da182a40e424bedf9
Tim Kormachev <tkormachev@alfabank.ru>
2020-03-26 19:52:52 +0300

    Lesson12:  saving results

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,14 +1,13 @@
 package ru.javawebinar.basejava;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.*;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private static int counter;
     private static final Object LOCK = new Object();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.out.println(Thread.currentThread().getName());
@@ -17,33 +16,51 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-               throw new IllegalStateException();
+                throw new IllegalStateException();
             }
         };
         thread0.start();
         new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
         System.out.println(thread0.getState());
         final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
 
-        List<Thread> treads = new ArrayList<>();
+//        List<Thread> treads = new ArrayList<>();
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() -> {
+//            Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
                 }
+                latch.countDown();
+                return counter;
             });
-            thread.start();
-            treads.add(thread);
+            System.out.println(future.isDone());
+            System.out.println(future.get());
+
+//            BAD CODE - too much consumption
+//            while(future.isDone()){
+//                Thread.sleep(100);
+//            }
+//            future.get();
+//            END OF BAD CODE
+
+//            thread.start();
+////          treads.add(thread);
 
         }
-        treads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+//        treads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
         System.out.println(counter);
     }
 
@@ -51,7 +68,7 @@ public class MainConcurrency {
 //        synchronized (this){
 //        synchronized (MainConcurrency.class)
 //        synchronizeded (thisis) {
-            counter++;
+        counter++;
 //        }
     }
 }
